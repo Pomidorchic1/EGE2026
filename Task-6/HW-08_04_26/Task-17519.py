08345c6e41211fe7f795abbae4d346c2b053bb04
akyan <akyantapli@gmail.com>
2026-04-07 23:44:10 +0300

    6 ЗАДАНИЕ,ЧЕРЕПАХА

---
+++ Task-6/HW-08_04_26/Task-17519.py
@@ -0,0 +1,36 @@
+from turtle import *
+
+screensize(10_000,10_000)
+tracer(False)
+lt(90)
+m = 10
+
+for i in range (9):
+    fd(22 * m)
+    rt(90)
+    fd(6 * m)
+    rt(90)
+
+up()
+fd(1 * m)
+rt(90)
+fd(5* m)
+lt(90)
+down()
+
+for i in range(9):
+    fd(53* m)
+    rt(90)
+    fd(75* m)
+    rt(90)
+down()
+up()
+for x in range (0,2):
+    for y in range (0,22):
+        goto (x * m, y * m)
+        dot(3,"red")
+
+update()
+done()
+
+print(2 * 22)
---
+++ Task-6/HW-08_04_26/Task-17625.py
@@ -0,0 +1,38 @@
+from turtle import *
+
+screensize(10_000, 10_000)
+tracer(False)
+lt(90)
+m = 10
+
+for i in range(10):
+    fd(22 * m)
+    rt(90)
+    fd(16 * m)
+    rt(90)
+
+up()
+
+fd(1 * m)
+rt(90)
+fd(1 * m)
+lt(90)
+
+down()
+
+for i in range(10):
+    fd(72 * m)
+    rt(90)
+    fd(79 * m)
+    rt(90)
+down()
+up()
+for x in range(1, 17):
+    for y in range(1, 23):
+        goto(x * m, y * m)
+        dot(3, "red")
+
+update()
+done()
+
+print((16 + 22) * 2 - 4)
