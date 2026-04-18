d88d3b19f41b46838bf4977da778464b123ef6de
Pomidorca <akyantaoli@gmail.com>
2026-04-18 10:49:06 +0200

    5  задание,функция convert

---
+++ Task-5/Task-9774.py
@@ -0,0 +1,20 @@
+def convert(num, sys):
+    res = ''
+    while num:
+        res += str(num % sys)
+        num //= sys
+    return res[::-1] if res else '0'
+
+
+ans = []
+
+for N in range(1, 100_000):
+    R = convert(N, 3)
+    if N % 3 == 0:
+        R = R + R[-2:]
+    else:
+        R = R + convert(N % 3 * 5, 3)
+    R = int(R,3)
+    if R > 133:
+        ans.append(R)
+print (min(ans))
