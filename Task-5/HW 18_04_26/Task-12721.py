66c5b6198a02875ad2fc5148ff84d529d24ace08
Pomidorca <akyantaoli@gmail.com>
2026-04-18 11:26:57 +0200

    Исправленная дзшка

--- Task-5/HW 18_04_26/Task-12721.py
+++ Task-5/HW 18_04_26/Task-12721.py
@@ -1,19 +1,20 @@
-def convert (num,sys):
+def convert(num, sys):
     res = ''
     while num:
         res += str(num % sys)
         num //= sys
-    return res [::-1] if res else '0'
+    return res[::-1] if res else '0'
+
 
 ans = []
 
-for N in range(1,100_000):
-    R = convert (N,8)
-    if R.count('2') + R.count('4') + R.count('6') % 2 != 0:
-        R = R + '46'
+for N in range(1, 100_000):
+    R = convert(N, 8)
+    if (R.count('0') + R.count('2') + R.count('4') + R.count('6')) % 2 != 0:
+        R = R[-3:] + '46'
     else:
-        R = convert(N % 8 * 5,8) + R
-    R = int(R,8)
+        R = convert(N % 8 * 5, 8) + R
+    R = int(R, 8)
     if N >= 80:
         ans.append(R)
 print(min(ans))
--- Task-5/HW 18_04_26/Task-22270.py
+++ Task-5/HW 18_04_26/Task-22270.py
@@ -10,6 +10,13 @@ ans = []
 for N in range(1, 10000):
     R = convert(N, 4)
     if R[0] == '3':
-        R = '1' = '3' and '3' = '1'
+        R = R.replace('1','*')
+        R = R.replace('3','1')
+        R= R.replace('*','3')
         R = '21' + R
     else:
+        R = '1' + R[1:] + '12'
+    R = int(R,4)
+    if R < 598:
+        ans.append([R,N])
+print(max(ans))
