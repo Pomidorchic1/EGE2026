92e3669ebdcbf87539aa42b6801c3c869ab71298
akyan <akyantapli@gmail.com>
2026-04-28 19:23:16 +0300

    5 и 14 задание ДЗ

---
+++ Task-14/HW 28_04_26/Task-23754.py
@@ -0,0 +1,8 @@
+for x in range(1, 3001):
+    num = 9 * 11 ** 210 + 8 * 11 ** 150 - x
+    cnt = 0
+    while num:
+        if num % 11 == 0: cnt+=1
+        num //= 11
+    if cnt == 60:
+        print(x)
