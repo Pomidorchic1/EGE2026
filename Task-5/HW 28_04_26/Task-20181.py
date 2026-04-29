eef1d8c55b34517a8cf976ed78b4620e9e1df1cd
Pomidorca <akyantaoli@gmail.com>
2026-04-29 16:04:54 +0200

    Исправленная домашка

--- Task-5/HW 28_04_26/Task-20181.py
+++ Task-5/HW 28_04_26/Task-20181.py
@@ -1,10 +1,10 @@
 ams = []
 for N in range(1,100_000):
     R = bin(N)[2:]
-    if R.count('1') % 2 == 0: R = R + bin(R.count('1'))[2:]
+    if N % 2 == 0: R = R + bin(R.count('1'))[2:]
     else:
         R = '1' + R + '101'
     R = int(R,2)
     if R > 350:
-        ams.append(R)
+        ams.append(N)
 print(min(ams))
